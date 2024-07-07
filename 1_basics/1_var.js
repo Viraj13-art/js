3b55cc74e75a690ba05c48ef38f8e5d3aba31221
Viraj13-art <virajsinhthakor13@gmail.com>
2024-07-07 13:06:28 +0000

    commit

---
+++ 1_basics/1_var.js
@@ -0,0 +1,18 @@
+const accountId = 9292929
+let accountEmail= "virajsinhthakor13@gmail.com"
+var accountPassword = "123445"
+accountCity= "surat"
+
+// accountId = 2 is not allowed/invalid (const is mentioned)
+console.log(accountId);
+
+/*
+prefer not to use var due to scope issues
+*/
+
+accountEmail="vtvtvt@goggle.com"
+accountPassword="292929292"
+accountCity="bharch"
+
+console.table([accountEmail,accountPassword,accountCity])
+
